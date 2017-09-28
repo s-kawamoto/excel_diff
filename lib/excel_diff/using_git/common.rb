dabe8aaa37eea8e80149d76836769549c5e8e288
satoshi kawamoto <kawamoto@netlab.jp>
2017-09-28 16:10:10 +0900

    指定したファイルがワーキングツリーのルートディレクトリからの相対パスに自動で変更されるように修正

--- lib/excel_diff/using_git/common.rb
+++ lib/excel_diff/using_git/common.rb
@@ -7,16 +7,32 @@ module ExcelDiff
       module_function
 
       def get_excel_commit_at(commit_id, file)
-        if system("git log #{commit_id}:#{file} >/dev/null")
+        exit unless in_repository?
+        file_path_from_toplevel = path_from_toplevel(file)
+
+        if system("git log #{commit_id}:#{file_path_from_toplevel} >/dev/null")
           tmp_dir = File.join(File.dirname(__FILE__), 'tmp')
           tmp_file_path = File.join(tmp_dir, commit_id + "_" + File.basename(file))
 
           FileUtils.mkdir_p(tmp_dir)
-          system("git show #{commit_id}:#{file} > #{tmp_file_path}")
+          system("git show #{commit_id}:#{file_path_from_toplevel} > #{tmp_file_path}")
         else
           exit
         end
         tmp_file_path
+      end
+
+      def path_from_toplevel(file)
+        require 'pathname'
+
+        toplevel_path = Pathname.new(`git rev-parse --show-toplevel`.chomp)
+        absolute_file_path = Pathname.new(File.absolute_path(file))
+        file_path_from_toplevel = absolute_file_path.relative_path_from(toplevel_path)
+        file_path_from_toplevel.to_s
+      end
+
+      def in_repository?
+        system('git rev-parse --show-toplevel >/dev/null')
       end
     end
   end
